37ccafa6b52a7e324183c00896c943e00c3f6796
Charan-Boddapati <charan259boddapati@gmail.com>
2024-06-27 09:06:45 +0530

    first commit

---
+++ python_stress/info.py
@@ -0,0 +1,6 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS =True
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST_USER = 'trackchamps@outlook.com'
+EMAIL_HOST_PASSWORD = 'CPP!@#123'
+EMAIL_PORT = 587
